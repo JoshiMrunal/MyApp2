0705f50f29ad7bd67c16f881ded8c75d9f614a9e
hp <jmrunal.15@gmail.com>
2017-11-20 12:28:36 +0530

    Changes are done in main activity.

--- app/src/main/java/com/example/hp/myapp/MainActivity.java
+++ app/src/main/java/com/example/hp/myapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.hp.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonstart = (Button) findViewById(R.id.buttonstart);
+
+        buttonstart.setOnClickListener(this);
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
     }
 }
